22069866fb5b3b9142fa90438b4526ed303c84d2
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 16:12:36 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,7 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    // Run the Dependency-Check scan with necessary arguments
+    dependencyCheck additionalArguments: '--scan ./ --out ./dependency-check-reports --format XML', odcInstallation: 'OWASP'
+
+    // Publish the Dependency-Check results
+    dependencyCheckPublisher pattern: '**/dependency-check-reports/dependency-check-report.xml'
 }
