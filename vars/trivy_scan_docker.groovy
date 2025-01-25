73a50eb4707483e6747cb4a07f191a44ed203453
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 20:08:01 +0530

    Update trivy_scan_docker.groovy

--- vars/trivy_scan_docker.groovy
+++ vars/trivy_scan_docker.groovy
@@ -1,14 +1,14 @@
-// vars/scanDockerImage.groovy
+// vars/trivy_scan_docker.groovy
 
 def call(imageName, VULN_FILE) {
     try {
-        // Run the Trivy scan for vulnerabilities and save output to a file
+        // Run the Trivy scan for vulnerabilities and save output to a file in table format
         echo "Running Trivy scan for image ${imageName}"
         def result = sh(script: """
             trivy image --format table --severity HIGH,CRITICAL --exit-code 0 --no-progress ${imageName} > ${VULN_FILE}
         """, returnStatus: true)
         
-        // If vulnerabilities are found (exit code 0, but vulnerabilities exist), log to a file
+        // Check if vulnerabilities were found based on the exit code
         if (result == 0) {
             echo "Vulnerabilities found, report saved to ${VULN_FILE}"
             return true // Return true indicating vulnerabilities were found
