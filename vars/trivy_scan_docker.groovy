0fd656e431ea5eff10bc75a12ed98a5760241192
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 20:14:02 +0530

    Update trivy_scan_docker.groovy

--- vars/trivy_scan_docker.groovy
+++ vars/trivy_scan_docker.groovy
@@ -1,23 +1,21 @@
-// vars/trivy_scan_docker.groovy
-
-def call(imageName, VULN_FILE) {
+def call(FS_VULN_FILE) {
     try {
-        // Run the Trivy scan for vulnerabilities and save output to a file in table format
-        echo "Running Trivy scan for image ${imageName}"
+        // Run the Trivy filesystem scan and save output to a file
+        echo "Running Trivy filesystem scan"
         def result = sh(script: """
-            trivy image --format table --severity HIGH,CRITICAL --exit-code 0 --no-progress ${imageName} > ${VULN_FILE}
+            trivy fs --format table --severity HIGH,CRITICAL --exit-code 0 --no-progress . > ${FS_VULN_FILE}
         """, returnStatus: true)
         
-        // Check if vulnerabilities were found based on the exit code
+        // If vulnerabilities are found (exit code 0, but vulnerabilities exist), log to a file
         if (result == 0) {
-            echo "Vulnerabilities found, report saved to ${VULN_FILE}"
+            echo "Filesystem vulnerabilities found, report saved to ${FS_VULN_FILE}"
             return true // Return true indicating vulnerabilities were found
         } else {
-            echo "No high or critical vulnerabilities found."
+            echo "No high or critical filesystem vulnerabilities found."
             return false // Return false indicating no vulnerabilities
         }
     } catch (Exception e) {
-        echo "Error during Trivy scan: ${e.getMessage()}"
+        echo "Error during filesystem scan: ${e.getMessage()}"
         return true // Assume vulnerabilities found if an error occurs
     }
 }
