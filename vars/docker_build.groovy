d14efb8a2b7b4b3ff3c9cec680d5220a5cc16fb2
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 16:28:37 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,7 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+// Define the function to build Docker image
+def docker_build(String projectName, String imageTag, String dockerHubUser) {
+    echo "Building Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
+    sh """
+        docker build -t ${dockerHubUser}/${projectName}:${imageTag} .
+    """
 }
