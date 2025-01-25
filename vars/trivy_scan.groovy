f39b8e8e4e4713330c80599ea51201a5adcec4a2
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 20:22:20 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,7 @@
-def call(){
-  sh "trivy fs ."
+def trivy_scan() {
+    // Run Trivy scan on the current directory
+    sh "trivy fs . --format plain --output trivy_scan_results.txt"
+    
+    // Archive the results file to make it available in Jenkins UI
+    archiveArtifacts artifacts: 'trivy_scan_results.txt', allowEmptyArchive: true
 }
