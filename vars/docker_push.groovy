96d53a6cd8ea1932270e2729a554fd9c84dc7335
Cgoyal-Developer <95074914+Cgoyal-Developer@users.noreply.github.com>
2025-01-25 18:06:58 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,7 @@
 // Define the function to push Docker image to Docker Hub
-def docker_push(String project, String imageTag, String dockerHubUser) {
-    echo "Pushing Docker image: ${dockerHubUser}/${project}:${imageTag}"
+def docker_push(String project, String imageTag, String dockerhubUser) {
     withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker login -u ${dockerhubUser} -p ${dockerHubPass}"
     }
-    sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+    sh "docker push ${dockerhubUser}/${project}:${imageTag}"
 }
